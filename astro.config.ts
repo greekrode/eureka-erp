69594677e8444c5a8f0ed7d1f9cf2e30ed759d66
Roderick Halim <kangritel@gmail.com>
2024-06-27 09:32:07 +0700

    add sitemap and robots

--- astro.config.ts
+++ astro.config.ts
@@ -1,16 +1,18 @@
-import { defineConfig } from 'astro/config';
-import tailwind from '@astrojs/tailwind';
-import vercelServerless from '@astrojs/vercel/serverless';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
+import vercelServerless from '@astrojs/vercel/serverless'
+import react from '@astrojs/react'
 
-import react from "@astrojs/react";
+import sitemap from '@astrojs/sitemap'
 
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercelServerless({
-    webAnalytics: {
-      enabled: true
-    }
-  }),
-  integrations: [tailwind(), react()]
-});
+	output: 'server',
+	adapter: vercelServerless({
+		webAnalytics: {
+			enabled: true
+		}
+	}),
+	site: 'https://eureka-erp.com',
+	integrations: [tailwind(), react(), sitemap()]
+})
