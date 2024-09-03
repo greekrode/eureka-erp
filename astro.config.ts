703c4e78c1e6058df37b7e2665553f0ac5301e0a
Roderick Halim <kangritel@gmail.com>
2024-09-03 22:53:16 +0700

    latest udpate

--- astro.config.ts
+++ astro.config.ts
@@ -1,18 +1,18 @@
 import { defineConfig } from 'astro/config'
 import tailwind from '@astrojs/tailwind'
-import vercelServerless from '@astrojs/vercel/serverless'
 import react from '@astrojs/react'
 import sitemap from '@astrojs/sitemap'
 import { shield } from '@kindspells/astro-shield'
 
 // https://astro.build/config
 export default defineConfig({
-	output: 'server',
-	adapter: vercelServerless({
-		webAnalytics: {
-			enabled: true
-		}
-	}),
+	output: 'static',
+	image: {
+		service: {
+			entrypoint: 'astro/assets/services/sharp'
+		},
+		remotePatterns: [{ protocol: 'https' }]
+	},
 	site: 'https://www.eureka-erp.com',
 	integrations: [tailwind(), react(), sitemap(), shield({})]
 })
